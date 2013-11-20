7d5056f993b1814ba754ac49da08e05725b23847
Clemens Sahs <clemens.sahs@slides-worker.org>
2013-11-20 11:03:20 +0100

    add travis ci testing

---
+++ tests/OcUserSqlTest/GeneralTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace OcUserSqlTest;
+
+use \PHPUnit_Framework_TestCase;
+
+class GeneralTest extends PHPUnit_Framework_TestCase
+{
+    public function setup ()
+    {}
+
+    public function testFooBar()
+    {
+        $this->assertTrue(true);
+    }
+}
